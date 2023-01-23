0430df3f0cfdc14f7261b3a4b5490b171d496675
Parker <pknopf@ucsd.edu>
2023-01-23 08:58:54 -0800

    Add sim code

---
+++ parameters.m
@@ -0,0 +1,14 @@
+% PARAMETERS - Belongs to MAELAB.M for MAE171a
+% MatLab template file for specifying 2DOF model parameters
+
+% Written by R.A. de Callafon, Dept. of MAE, UCSD (2001-2021)
+% Report errors in this software to <callafon@ucsd.edu>
+
+m1=1e-6;         % Mass/inertia m1                     
+d1=4e-6;         % Damping that connects m1 to ground  
+k1=2e-4;         % Spring that connect m1 to ground
+
+m2=1e-6;         % Mass/inertia m2                     
+d2=1e-6;         % Damping that connects m2 to ground 
+k2=1e-4;         % Spring that connects m1 and m2      
+
