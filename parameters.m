0de042964911f35bb8dc133b7434640394454844
Parker <pknopf@ucsd.edu>
2023-01-24 21:41:29 -0800

    updated parameters file

--- parameters.m
+++ parameters.m
@@ -4,11 +4,11 @@
 % Written by R.A. de Callafon, Dept. of MAE, UCSD (2001-2021)
 % Report errors in this software to <callafon@ucsd.edu>
 
-m1=1e-6;         % Mass/inertia m1                     
-d1=4e-6;         % Damping that connects m1 to ground  
-k1=2e-4;         % Spring that connect m1 to ground
+m1=2.587;         % Mass/inertia m1                     
+d1=1.437;         % Damping that connects m1 to ground  
+k1=25.77;         % Spring that connect m1 to ground
 
-m2=1e-6;         % Mass/inertia m2                     
-d2=1e-6;         % Damping that connects m2 to ground 
-k2=1e-4;         % Spring that connects m1 and m2      
+m2=2.587;         % Mass/inertia m2                     
+d2=1.437;         % Damping that connects m2 to ground 
+k2=25.77;         % Spring that connects m1 and m2      
 
